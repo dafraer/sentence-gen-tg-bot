760edcacbbae711af47dcb37fd88358b691cd38a
Kamil <kdnuriev@gmail.com>
2025-03-16 14:37:53 +0400

    added gemini client

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"os"
 
+	"github.com/dafraer/sentence-gen-tg-bot/gemini"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		panic("API key must be passed aas an argument")
+	}
+	ctx := context.TODO()
+	client, err := gemini.New(ctx, os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err := client.Close(); err != nil {
+			panic(err)
+		}
+	}()
+
+	resp, err := client.Request(ctx, "Give me a simple A1 sentence in russian with the word сосать. dont explain me anything just give me the sentence and trsnalation seperated by ; symbol")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(resp)
 }
--- gemini/client.go
+++ gemini/client.go
@@ -1,4 +1,56 @@
 package gemini
 
+import (
+	"context"
+	"strings"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+const geminiFlash = "gemini-1.5-flash"
+
 type Client struct {
+	client *genai.Client
+}
+
+// New creates new gemini client
+func New(ctx context.Context, token string) (*Client, error) {
+	client, err := genai.NewClient(ctx, option.WithAPIKey(token))
+	if err != nil {
+		return nil, err
+	}
+	return &Client{client: client}, nil
+}
+
+// Close closes the client
+func (c *Client) Close() error {
+	if err := c.client.Close(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Request sends a text-only request to the gemini-1.5-flash model
+func (c *Client) Request(ctx context.Context, request string) (string, error) {
+	//Specify model
+	model := c.client.GenerativeModel(geminiFlash)
+
+	//Generate content
+	resp, err := model.GenerateContent(ctx, genai.Text(request))
+	if err != nil {
+		return "", err
+	}
+
+	//Extract response
+	var response strings.Builder
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				response.WriteString(string(part.(genai.Text)))
+			}
+		}
+	}
+
+	return response.String(), nil
 }
